6ea25410fad387a2629d49848c02408f45cd3a02
IJS1016 <imjungsunee@gmail.com>
2021-04-11 19:16:24 +0900

    Study C++

---
+++ C++/Study/pointer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int n = 10;
+    int *nPtr;
+
+    nPtr = &n; // n의 메모리 주소를 포인터 변수에 저장
+
+    std::cout<<"*nPtr : "<<*nPtr<<"\n"; // 포인터 앞에 *를 붙임으로서 해당 메모리에 저장된 값을 가져옴
+    std::cout<<"n     : "<<n<<"\n";
+    
+    //std::cout<<"&n    : "<<&n<<"\n";    // n 변수 주소값을 나타냄
+    
+    
+    *nPtr = 20;
+
+    std::cout<<"*nPtr : "<<*nPtr<<"\n";
+}
