7fd013dd2806f5a348351f17858294131e49877f
IJS1016 <imjungsunee@gmail.com>
2021-04-15 22:31:40 +0900

    Practice Algorithm

--- C++/Study/pointer.cpp
+++ C++/Study/pointer.cpp
@@ -11,8 +11,7 @@ int main(void){
     std::cout<<"*nPtr : "<<*nPtr<<"\n"; // 포인터 앞에 *를 붙임으로서 해당 메모리에 저장된 값을 가져옴
     std::cout<<"n     : "<<n<<"\n";
     
-    //std::cout<<"&n    : "<<&n<<"\n";    // n 변수 주소값을 나타냄
-    
+    //std::cout<<"&n    : "<<&n<<"\n";    // n 변수 주소값을 나타냄  
     
     *nPtr = 20;
 
