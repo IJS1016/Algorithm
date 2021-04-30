edad15f72d4d5a4d97a0aaf1bd2d3505f7425016
IJS1016 <imjungsunee@gmail.com>
2021-04-30 11:00:28 +0900

    HAVE TO SOLVE 1439.cpp on Greedy

---
+++ C++/BOJ/greedy/1439.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+/*뒤집기 - https://www.acmicpc.net/problem/1439*/
+int main(void){
+    int S_LEN = 1000000;
+    char s[S_LEN];
+
+    std::cin>>s;
+
+    for (int i=0; i < S_LEN; i++){
+        std::cout<<s[i];
+        if (s[i] == NULL){
+            break;
+        }
+    }
+
+}
