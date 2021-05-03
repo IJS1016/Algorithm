6d33deaf532153a0afb47fd382e21036e7e8498a
IJS1016 <imjungsunee@gmail.com>
2021-05-03 11:29:39 +0900

    HAVE TO SOLVE 1037.cpp

---
+++ C++/BOJ/math/1037.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+/*
+*/
+
+int main(void){
+    int n;
+    int divisors[n];
+
+    for (int i=0; i<n; i++){
+        std::cin>>divisors[i];
+    }
+    
+}
+int get_divisor(int num){
+    
+}
