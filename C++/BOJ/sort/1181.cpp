ca0e2f01f060e575828c96d1783f69359bc08027
IJS1016 <imjungsunee@gmail.com>
2021-05-09 21:06:51 +0900

    Practice Algorithm

--- C++/BOJ/sort/1181.cpp
+++ C++/BOJ/sort/1181.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <cstring>
 using namespace std;
 
@@ -10,7 +11,25 @@ OutOfBound Error
 
 210506 코드는 돌지만 시간초과
 -> 오답 : sort 방법 공부 & 변경 해보기
+
+210509 알고리즘 풀이 때 sort 함수를 사용하는 것이 좋음
+string이 길이를 동적할당하지만, strcmp와 같은 함수를 사용하지 않아도 되서 직관적
+string 길이 a.length()
+
+sort 함수 사용법을 익혔다
+comp에 sort 하는 조건들을 넣으면 쉽게 sort가 가능하다!
+
+드디어 풀었네...
+
 */
+bool comp(string a, string b){
+    if (a.length() == b.length()){
+        return a < b;
+    }
+    else {
+        return a.length() < b.length();
+    }
+}
 
 int main(void){
     ios::sync_with_stdio(false);
@@ -21,7 +40,7 @@ int main(void){
 
     std::cin>>n;
 
-    char words[n+1][51], word[51];
+    string words[n+1], word;
 
     // get words except overlap words
     for (int i = 0; i < n; i++){
@@ -30,7 +49,7 @@ int main(void){
         std::cin>>word;
 
         for (int j = 0; j < i; j++){
-            if (!strcmp(words[j], word)){
+            if (words[j] == word){
                 flag = 1;
                 i = i-1;
                 n = n-1;
@@ -38,37 +57,11 @@ int main(void){
             }
         }
         if (flag == 0){
-            strcpy(words[i], word);
+            words[i] =  word;
         }
     }
 
-    // sort words
-    for (int i = 0; i < n; i++){
-        char temp[51];
-
-        for (int j=0; j < n-i-1; j++){
-            if (strlen(words[j]) > strlen(words[j+1])){
-                strcpy(temp, words[j]);
-                strcpy(words[j], words[j+1]);
-                strcpy(words[j+1], temp);
-            }
-            if (strlen(words[j]) == strlen(words[j+1])){
-                int word_length = strlen(words[j]);
-
-                for (int k = 0; k < word_length; k++){
-                    if (words[j][k] < words[j+1][k]){
-                        break;
-                    }
-                    if (words[j][k] > words[j+1][k]){
-                        strcpy(temp, words[j]);
-                        strcpy(words[j], words[j+1]);
-                        strcpy(words[j+1], temp);
-                        break;
-                    }
-                }
-            }
-        }
-    }
+    sort(words, words+n, comp);
 
     for (int i=0; i < n; i++){
         std::cout<<words[i]<<"\n";
