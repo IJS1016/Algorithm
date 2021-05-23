5c095f02317a62df6b448498db5fcbbff8cee9c1
IJS1016 <imjungsunee@gmail.com>
2021-05-23 23:39:59 +0900

    Practice Algorithm

--- C++/BOJ/implement/2908.cpp
+++ C++/BOJ/implement/2908.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
-#include <cmath>
+#include <cstring>
 using namespace std;
 
+/* 상수 https://www.acmicpc.net/problem/2908 */
+// char를 선언해 풀이한 경우
+// std::cin>>a>>b;를 사용하면 a를 받아오지 못한다, 하지마 b, a 순으로 변경하면 잘 된다 왜??
+
 int main(void){
-    int a, b, tmp_a=0, tmp_b=0, i_10;
+    string a, b, tmp_a="000", tmp_b = "000";
+
+
     std::cin>>a>>b;
 
-    for (int i=3; i>= 0; i++){
-        
-
-        tmp_a += (a % (pow(10, i)) * (pow(10, (3-i))));
-        tmp_b += (b % (pow(10, i)) * (pow(10, (3-i))));
-        
+    for (int i=0; i<3; i++){
+        tmp_a[i] = a[2-i];
+        tmp_b[i] = b[2-i];
     }
 
-    if (tmp_a > tmp_b) {
+    if (stoi(tmp_a) > stoi(tmp_b)) {
         std::cout<<tmp_a;
     }
     else {
