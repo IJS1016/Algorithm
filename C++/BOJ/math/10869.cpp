0dd1c50f145e3c1ea4c1e813ddf1ab18101ac240
IJS1016 <imjungsunee@gmail.com>
2021-05-25 09:19:06 +0900

    Practice Algorithm

---
+++ C++/BOJ/math/10869.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int a, b;
+
+    std::cin>>a>>b;
+
+    std::cout<<a+b<<"\n";
+    std::cout<<a-b<<"\n";
+    std::cout<<a*b<<"\n";
+    std::cout<<a/b<<"\n";
+    std::cout<<a%b<<"\n";
+
+}
