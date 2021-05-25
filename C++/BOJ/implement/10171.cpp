07c8b72fa86bc5902bbba37c8da27ae976ad8567
IJS1016 <imjungsunee@gmail.com>
2021-05-25 09:00:48 +0900

    Practice Algorithm

---
+++ C++/BOJ/implement/10171.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void){
+    string cat[] = {"\\    /\\\n", 
+                    " )  ( ')\n",
+                    "(  /  )\n",
+                    " \\(__)|\n"};
+
+    for (int i=0; i<4; i++){
+        std::cout<<cat[i];
+    }
+
+}
