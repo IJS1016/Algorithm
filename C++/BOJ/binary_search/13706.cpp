c4ad16d551099b6e82cf95c0a1fecfd0322fa074
IJS1016 <imjungsunee@gmail.com>
2021-06-04 13:23:44 +0900

    Practice Algorithm

--- C++/BOJ/binary_search/13706.cpp
+++ C++/BOJ/binary_search/13706.cpp
@@ -11,68 +11,62 @@ string으로 승부를 본다
 string을 또 쪼개서 int로 만든 다음 제곱한다
 더하는거 해결해야 함*/
 
+// str_sum reminder 올림한 경우 생각해야 함 
 string str_sum(string a, string b){
-    // 더하는 거 걍 1의 자리수로 졸라게 더하기 그게 쉬울듯 아무리 많이 돌아도
-    int split = 3;
-    int temp=0, max_len;
-    string stemp, result, zeros;
-
-    int a_len = a.length(), b_len = b.length();
-
-    if (a_len > b_len) {
-        max_len = a_len;
-    }
-    else {
-        max_len = b_len;
-    }
-    std::cout<<"STR_SUM "<<a<<" "<<b<<"\n";
-
+    int min_len, temp=0;
+    string remainder, result;
+    
     reverse(a.begin(), a.end());
     reverse(b.begin(), b.end());
 
-    for (int i=0; i <max_len; i+=split){
-        zeros = "";
-        std::cout<<"FOR "<<a.substr(i, i+split)<<" "<<b.substr(i, i+split)<<"\n";
-        if (a_len < i){
-            result += b.substr(i, b_len-i);
-            break;
+    int a_len = a.length(), b_len = b.length();
+    //std::cout<<"LEN "<<a_len<<" "<<b_len<<" "<<remainder.length()<<"\n";
+
+    if (a_len <= b_len) {
+        min_len = a_len;
+        remainder = b.substr(a_len, b_len-a_len);
+    }
+    else {
+        min_len = b_len;
+        remainder = a.substr(b_len, a_len-b_len);
+    }
+
+    for (int i=0; i<min_len; i++){
+        std::cout<<"I"<<i<<"\n";
+        temp += a[i] + b[i] - '0' * 2;
+        if (temp < 0) {
+            temp = 0;
         }
-        else if (b_len < i){
-            result += a.substr(i, a_len-i);
-            break;
-        }
+        std::cout<<"TEMP "<<temp<<"  a,b "<<a[i]<<" "<<b[i]<<"\n";
 
-        temp += stoi(a.substr(i, i+split) + b.substr(i, i+split));
-        stemp = to_string(temp);
+        result += to_string(temp % 10);
+        std::cout<<"TEMP0 "<<to_string(temp % 10)<<" result : "<<result<<"\n\n";
 
-        reverse(stemp.begin(), stemp.end());
-
-        std::cout<<"STEMP "<<stemp<<"\n";
-
-        if (stemp.length() > split) {
-            result += stemp.substr(1, stemp.length());
+        if (temp / 10 > 0){
             temp = 1;
         }
-        else if (stemp.length() < split) {
-            for (int j=0; j<split-stemp.length(); j++){
-                zeros += '0';
-            }
-            result += stemp + zeros;
-        }
         else {
-            result += stemp;
+            temp = 0;
         }
     }
 
+
+    if (temp == 1){
+        if (remainder.length() != 0) {
+            int digit_up = int(remainder[0]) - int('0') + 1;
+            remainder = to_string(digit_up) + remainder.substr(1, remainder.length()-1);
+        }
+        else {
+            remainder += '1';
+        }
+    }
+
+    result += remainder;
     reverse(result.begin(), result.end());
-    std::cout<<"RESULT "<<result<<"\n";
-
-
     return result;
 }
 
 string squared(string n){
-    """큰 수 제곱하는 함수 using string"""
     string result="0", smulti, zeros;
     int split = 3, multi, n_size = n.length(), int_result;
  
@@ -80,9 +74,14 @@ string squared(string n){
 
     for (int i=0; i <n_size; i += split){ // 끊어서 곱한 뒤에 더 해주기
         for (int j=n_size-1; j >= 0; j--){
-            multi = stoi(n.substr(i, split)) * stoi(n.substr(j, 1));
-            // std::cout<<"i "<<i<<" "<<i+split<<", j "<<j<<"\n";
-            // std::cout<<"MULTI "<<stoi(n.substr(i, split))<<" * "<<stoi(n.substr(j, 1))<<"\n";
+            if (i + split > n_size) {
+                multi = stoi(n.substr(i, i + split - n_size)) * stoi(n.substr(j, 1));
+            }
+            else {
+                multi = stoi(n.substr(i, split)) * stoi(n.substr(j, 1));
+            }
+            std::cout<<"i "<<i<<" "<<i+split<<", j "<<j<<"\n";
+            std::cout<<"MULTI "<<stoi(n.substr(i, split))<<" * "<<stoi(n.substr(j, 1))<<"\n";
 
             int range = (n_size-(i+split));
 
@@ -90,9 +89,9 @@ string squared(string n){
                 range = 0;
             }
 
-            // std::cout<<"I : "<<(n_size-(i+split))<<" J : "<<(n_size-j-1)<<"\n";
-            for (int k=0; k < (range)+(n_size-j-1); k++){
-                zeros += '0';
+            std::cout<<"I : "<<(n_size-(i+split))<<" J : "<<(n_size-j-1)<<"\n";
+            for (int k = 0; k < (range)+(n_size-j-1); k++){
+                //zeros += '0';
                 multi = multi * 10;
             }
 
@@ -103,7 +102,8 @@ string squared(string n){
             result = str_sum(result, smulti);
 
             int_result += multi;
-            std::cout<<"MUL "<<multi<<"\n\n";
+            std::cout<<"RESULT "<<result<<"\n";
+            std::cout<<"INT "<<int_result<<"\n\n";
        }
     }
     std::cout<<int_result<<"\n";
@@ -114,7 +114,6 @@ string squared(string n){
 int main(void){
     string n;
     std::cin>>n;
-
 
     // 이진탐색
     squared(n);
--- C++/BOJ/binary_search/1654.cpp
+++ C++/BOJ/binary_search/1654.cpp
@@ -8,69 +8,42 @@ using namespace std;
 첫번째 풀이 : 시간 초과 -> 이분탐색 방법 공부하고 이용해보기 */
 
 
-
-'''
-//첫번쨰 풀이 -> 시간초과
 int main(void){
-    int b_n, a_n, lines[10000];
+    int k, n, lines[10000];
+    int middle, start=0, end=0, result;
 
-    // input parameters
-    std::cin>>b_n>>a_n;
-    
-    for (int i=0; i<b_n; i++){
+    std::cin>>k>>n;
+
+    for (int i=0; i<k; i++){
         std::cin>>lines[i];
-    }
-
-    int cnt = a_n - b_n;
-    int cut[10000]={0, }, split_line[10000];
-    int longest, shorest_after_cut = -1, max_idx;
-    copy(lines, lines+b_n, split_line);
-
-
-    while (cnt > 0){
-        while (cnt > 0) {
-            max_idx = 0;
-            longest = lines[0] / (cut[0]+2);
-
-            for (int i=1; i<b_n; i++){
-                if (longest < (lines[i] / (cut[i]+2))){
-                    longest = (lines[i] / (cut[i]+2));
-                    max_idx = i;
-                }
-            }
-
-            cut[max_idx] += 1;
-            split_line[max_idx] = lines[max_idx] / (cut[max_idx]+1);
-
-            cnt -= 1;
-        }
-
-        shorest_after_cut = -1;
-
-        for (int i=0; i<b_n; i++){
-            if (cut[i] != 0) {
-                if (shorest_after_cut == -1){
-                    shorest_after_cut = (split_line[i]/(cut[i]+2));
-                }
-                else if (shorest_after_cut > (split_line[i]/(cut[i]+2)) ){
-                    shorest_after_cut = (split_line[i]/(cut[i]+2));
-                }
-            }
-        }
-
-        for (int i=0; i<b_n; i++){
-            if (cut[i] == 0) {
-                if (lines[i] < shorest_after_cut){
-                    for (int j=i; j<b_n; j++){
-                        split_line[i] = split_line[i+1];
-                    }
-                    b_n -= 1;
-                    cnt += 1;
-                }
-            }
+        if (end < lines[i]){
+            end = lines[i];
         }
     }
 
-    std::cout<<*min_element(split_line, split_line+b_n);
+    int cnt = 0;
+
+    while (start < end) {
+        cnt = 0;
+        middle = (start + end) / 2;
+
+        std::cout<<cnt<<"->"<<start<<" "<<middle<<" "<<end<<"\n";
+
+        for (int i=0; i<k; i++){
+            cnt += lines[i] / middle;
+        }
+        if (cnt < n) {
+            end = middle;
+        }
+        else if (cnt > n) {
+            start = middle;
+        }
+        else {
+            result = middle;
+            start  = middle+1;
+        }
+        //std::cout<<cnt<<"->"<<start<<" "<<end<<"\n";
+    }
+    //std::cout<<cnt<<"->"<<start<<" "<<end<<"\n";
+    std::cout<<result;
 }
-'''
