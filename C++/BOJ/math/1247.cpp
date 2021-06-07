7f74ac1057c5cbf4e7e582ddb72ef0708dc21de5
IJS1016 <imjungsunee@gmail.com>
2021-06-08 08:50:29 +0900

    Practice Algorithm

--- C++/BOJ/math/1247.cpp
+++ C++/BOJ/math/1247.cpp
@@ -1,44 +1,56 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
-//overflow에 대해서 생각하는 것이 중요
+
+/* 부호 https://www.acmicpc.net/problem/1247
+if 문에서 overflow 발생 시 undefined behavior에 해당되 자동으로 false 처리
+LONG_MAX, LONG_MIN 이용해서 overflow를 안 일으키고 검사하는 것이 포인트
+https://www.acmicpc.net/board/view/56858 */
+
 
 int main(void){
-    long long int result=0, overflow=0;
+    long long result=0, overflow=0;
     int n;
 
-    std::cin>>n;
+    for (int test_case=0; test_case<3; test_case++){
+        std::cin>>n;
+        
+        result   = 0;
+        overflow = 0;
 
-    for (int i=0; i < n; i++){
-        long long int tmp;
-        std::cin>>tmp;
+        for (int i=0; i < n; i++){
+            long long tmp;
+            std::cin>>tmp;
 
-        if (result > 0 and tmp > 0 and result + tmp <= 0) {
-            //result = result + tmp - LLONG_MIN;
-            overflow += 1;
+            if (result > 0 and tmp > 0 and tmp > LLONG_MAX - result) {
+                //result = result + tmp - LLONG_MIN;
+                overflow += 1;
+            }
+
+            if (result < 0 and tmp < 0 and tmp < LONG_MIN - result) {
+                //result = result + tmp - LLONG_MAX;
+                overflow -= 1;
+            }
+
+            result += tmp;
         }
 
-        if (result < 0 and tmp < 0 and result + tmp >= 0) {
-            //result = result + tmp - LLONG_MAX;
-            overflow -= 1;
+        if (overflow < 0){
+            std::cout<<"-\n";
         }
-
-        result += tmp;
+        else if (overflow > 0){
+            std::cout<<"+\n";
+        }
+        else if (result==0){
+            std::cout<<"0\n";
+        }
+        else if (result < 0){
+            std::cout<<"-\n";
+        }
+        else if (result > 0){
+            std::cout<<"+\n";
+        }
     }
 
-    if (overflow < 0){
-        std::cout<<'-';
-    }
-    else if (overflow > 0){
-        std::cout<<'+';
-    }
-    else if (result==0){
-        std::cout<<result;
-    }
-    else if (result < 0){
-        std::cout<<'-';
-    }
-    if (result > 0){
-        std::cout<<'+';
-    }
 }
