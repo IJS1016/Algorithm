380cd3c8fed2d9d38522556f5942a2542230a6de
IJS1016 <imjungsunee@gmail.com>
2021-06-15 00:00:55 +0900

    Practice Algorithm

--- C++/BOJ/stack/10828.cpp
+++ C++/BOJ/stack/10828.cpp
@@ -4,55 +4,59 @@
 
 using namespace std;
 
+/* 10828 스택 -> print로 풀이시 에러 -> 해결 방법 생각해보기 
+                cout을 사용할 때도 top을 입력하면 error 발생
+                => 오답 : 변수 선언을 반복문 안에다 해서 그런 것임..^^
+                => c++ stack을 사용시, stack이 비어있을 때 pop, top 사용시 프로그램이 종료 되어 버림 */
+
+
+
 int main(void) {
     int N;
+
+    int value;
+    string opt;
+
+    stack<int> stack;
 
     scanf("%d", &N);
 
     for (int i=0; i<N; i++){
-        int value;
-        string opt;
-
-        stack<int> s;
-
-        getline(cin, opt);
+        cin>>opt;
 
         if (opt == "push") {
-            scanf("%d", &value);
-            s.push(value);
+            cin>>value;
+            stack.push(value);
         }
 
         else if (opt == "pop") {
-             if (s.empty()) {
-                printf("%d\n", s.top());
-                s.pop();
+             if (stack.empty()) {
+                cout<<"-1\n";
                 continue;
             }
-            printf("-1\n");
+            cout<<stack.top()<<"\n";
+            stack.pop();
         }
 
         else if (opt == "size") {
-            printf("%lu\n", s.size());
+            cout<<stack.size()<<"\n";
         }
 
         else if (opt == "empty") {
-            if (s.empty()) {
-                printf("0\n");
+            if (stack.empty()) {
+                cout<<"1\n";
                 continue;
             }
-            printf("1\n");
+            cout<<"0\n";
         }
 
         else if (opt == "top") {
-            if (s.empty()) {
-                printf("%d\n", s.top());
+            if (stack.empty()) {
+                cout<<"-1\n";
                 continue;
             }
-            printf("-1\n");
+            cout<<stack.top()<<"\n";
         }
-
     }
-
-
     return 0;
 }
