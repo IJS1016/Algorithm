690d45dd9036d1d0a88e50afefc50c2708e28fd1
IJS1016 <imjungsunee@gmail.com>
2021-06-20 21:02:19 +0900

    Practice Algorithm

---
+++ C++/BOJ/brute_force/2231.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main(void) {
+    int n, m;
+
+    std::cin>>n;
+
+    int tmp=n, check_n, digits=0, plus=0;
+
+    while (tmp > 0){
+        tmp = tmp / 10; 
+        digits += 1;
+    }
+
+
+    while (plus < digits * 9){
+        m = n - digits * 9 + plus;
+        tmp     = m;
+        check_n = m;
+
+        while (tmp > 0) {
+            if (check_n > n){
+                break;
+            }
+            check_n += tmp % 10;
+            tmp = tmp / 10;
+        }
+
+        if (check_n == n){
+            std::cout<<m<<"\n";
+            return 0;
+        }
+
+        plus += 1;
+    }
+
+    std::cout<<"0\n";   
+
+    return 0;
+}
