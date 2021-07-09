e56922a168fef111fc2c68ad30d44ce58815bafb
IJS1016 <imjungsunee@gmail.com>
2021-07-09 12:19:57 +0900

    Practice Algorithm

---
+++ C++/BOJ/sort/11650.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+/* 좌표 정렬하기 https://www.acmicpc.net/problem/11650 */
+
+using namespace std;
+
+bool cmp(pair<int, int> a, pair<int, int> b){
+    if (a.first < b.first) {
+        return true;
+    }
+
+    else if (a.first == b.first) {
+        if (a.second < b.second) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main(void) {
+    int n;
+
+    vector<pair<int, int> > vectors;
+
+    scanf("%d", &n);
+
+    int a, b;
+
+    for (int i=0; i < n; i++){
+        scanf("%d %d", &a, &b);
+        vectors.push_back(pair<int, int>(a, b));
+    }
+
+    sort(vectors.begin(), vectors.end(), cmp);
+
+    for (int i=0; i < n; i++){
+        printf("%d %d\n", vectors[i].first, vectors[i].second);
+    }
+
+    return 0;
+}
