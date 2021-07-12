84e6f93bde5cf7bc8a1b4cbafa678331c57c71ae
IJS1016 <imjungsunee@gmail.com>
2021-07-12 12:50:19 +0900

    Practice Algorithm

---
+++ C++/BOJ/data_structure/10866.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <cstring>
+
+void empty(int size) {
+    if (size == 0) {
+        printf("1\n");
+    }
+    else {
+        printf("0\n");
+    }
+}
+
+int front(int deque[], int size) {
+    if (size == 0) {
+        printf("-1\n");
+        return -1;
+    }
+    printf("%d\n", deque[0]);
+    return 1;
+}
+
+void back(int deque[], int size) {
+    if (size == 0) {
+        printf("-1\n");
+    }
+    else {
+        printf("%d\n", deque[size-1]);
+    }
+}
+
+void push_front(int deque[], int x, int size) {
+    // printf("front 1 {");
+    // for (int i=0; i < size+1; i++) {
+    //     printf("%d, ", deque[i]);
+    // }
+    // printf("}\n");
+
+    for (int i = size; i > 0; i--) {
+        deque[i] = int(deque[i-1]);
+    }
+
+    deque[0] = x;
+
+    // printf("front 2 {");
+    // for (int i=0; i < size+1; i++) {
+    //     printf("%d, ", deque[i]);
+    // }
+    // printf("}\n");
+}
+
+void push_back(int deque[], int x, int size) {
+    deque[size] = x;
+}
+
+void pop_front(int deque[], int size) {
+    if (!(front(deque, size) == -1)) {
+        for (int i=1; i < size; i++){
+            deque[i-1] = deque[i];
+        }
+    }
+}
+
+void pop_back(int deque[], int size) {
+    if (!(front(deque, size) == -1)) {
+        deque[size-1] = 0;
+    }
+}
+
+int main(void) {
+    int deque[100000];
+    int size = 0, n, x;
+    char opt[11];
+
+    scanf("%d", &n);
+
+    for (int i=0; i < n; i++) {
+        scanf("%s", opt);
+
+        if (!strcmp(opt, "push_front") or !strcmp(opt, "push_back")) {
+            scanf("%d", &x);
+            if (!strcmp(opt, "push_front")) {
+                push_front(deque, x, size);
+            }
+            else {
+                push_back(deque, x, size);
+            }
+            size += 1;
+        }
+
+        else if (!strcmp(opt, "pop_front") or !strcmp(opt, "pop_back")) {
+            if (!strcmp(opt, "pop_front")) {
+                pop_front(deque, size);
+            }
+            else {
+                pop_back(deque, size);
+            }
+
+            if (size != 0) {
+                size -= 1;
+            }
+        }
+
+        else if (!strcmp(opt, "front")) {
+            front(deque, size);
+        }
+        
+        else if (!strcmp(opt, "back")) {
+            back(deque, size);
+        }
+
+        else if (!strcmp(opt, "size")) {
+            printf("%d\n", size);
+        }
+        
+        else if (!strcmp(opt, "empty")) {
+            empty(size);
+        }
+
+        // printf("{");
+        // for (int i=0; i < size; i++) {
+        //     printf("%d, ", deque[i]);
+        // }
+        // printf("}\n");
+    }
+
+    return 0;
+
+}
---
+++ C++/BOJ/data_structure/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+        printf("{");
+        for (int i=0; i < size; i++) {
+            printf("%d, ", deque[i]);
+        }
+        printf("}\n");
