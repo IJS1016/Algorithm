fb28ac6f84f2e42fa03fe91d030f68d8d61dd6f9
IJS1016 <imjungsunee@gmail.com>
2021-11-12 08:15:43 +0900

    1225 이상한 곱셈 solved!

--- Python/BOJ/simulation/1225.py
+++ Python/BOJ/simulation/1225.py
@@ -1,11 +1,20 @@
-# 에러 발생
-
-import numpy as np
-
+# 1225 이상한 곱셈
+# https://www.acmicpc.net/problem/1225
 nums = input().split(" ")
 
-num1 = np.array(list(nums[0])).astype("int64")
-num2 = np.array(list(nums[0])).astype("int64")
-result = np.sum(num1 * num2)
+num1 = list(map(int, list(nums[0])))
+num2 = list(map(int, list(nums[1])))
+
+if len(num1) > len(num2) :
+    num_sum = sum(num1) 
+    num_mul = num2
+else :
+    num_sum = sum(num2)
+    num_mul = num1
+
+result = 0
+
+for n in num_mul :
+    result += num_sum * n
 
 print(result)
