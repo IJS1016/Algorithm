a0874a5454200651213b3bd2d2ff6926624264e0
IJS1016 <imjungsunee@gmail.com>
2021-11-13 21:03:40 +0900

    1158 런타임 에러 (IndexError)

---
+++ Python/BOJ/1158.py
@@ -0,0 +1,24 @@
+n, k = input().split(" ")
+#n, k = 7, 3
+
+n = int(n)
+k = int(k)
+
+array = [x for x in range(n)]
+result = []
+
+idx = k
+
+while len(array) > 0 :
+    result.append(array.pop(idx))
+    if len(array) == 0 :
+        break
+    idx = (idx + k - 1) % len(array)
+    
+for i in range(n) :
+    if result[i] == 0 :
+        result[i] = n
+        break
+
+print(f"<{', '.join(map(str, result))}>")
+
